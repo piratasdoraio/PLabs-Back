d842e8924262aeb8ae89bb6f19d52323a0f124b1
jdavicp <jdavi.fac@gmail.com>
2022-03-08 16:37:43 -0300

    2 new card endpoint

--- src/cards/card.controller.ts
+++ src/cards/card.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, HttpStatus, NotFoundException, Param, ParseIntPipe, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Get, HttpStatus, NotFoundException, Param, ParseIntPipe, Post } from "@nestjs/common";
 import { ApiResponse } from "@nestjs/swagger";
 import { CardDto } from "src/dtos/card.dto";
+import { CreateCardDto } from "src/dtos/create-card.dto";
 import { HttpExceptionDto } from "src/dtos/http-exception.dto";
 import { CardService } from "./card.service";
 
@@ -19,5 +20,16 @@ export class CardController {
         }
 
         return card;
+    }
+
+    @Post()
+    @ApiResponse({status: HttpStatus.CREATED, type: CardDto})
+    @ApiResponse({status: HttpStatus.BAD_REQUEST, type: HttpExceptionDto})
+    async createCard(@Body() createCardDto: CreateCardDto): Promise<CardDto> {
+        const newCard = await this.cardService.createCard(createCardDto);
+        if(!newCard) {
+            throw new BadRequestException();
+        }
+        return newCard;
     }
 }
